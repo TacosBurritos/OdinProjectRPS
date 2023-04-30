6a2716771b09d1a65277089a748ce2e48ffd6d0a
Takafumi Mashiko <tmashiko7@gmail.com>
2023-04-29 20:32:32 -0400

    added the score system

--- rps.js
+++ rps.js
@@ -1,5 +1,3 @@
-
-
 //computer selection rng for rock, paper, and scissors
 function getComputerChoice() {
     let rpsNumber = Math.floor(Math.random() * 3);
@@ -15,12 +13,6 @@ function getComputerChoice() {
 }
 //rules for single round of Rock paper scissors
 function playRound(playerSelection, computerSelection) {
-    //making sure the player input is valid
-    //** for some reason the code gets stuck here, check logic**/
-    /*if (playerSelection !== 'rock' || playerSelection !== 'scissors' || playerSelection !== 'paper') {
-        console.log('Please enter either a rock, scissors, or paper')
-    }*/
-    //in case of tie
     if (playerSelection === computerSelection) {
         console.log("It's a draw!");
     }
@@ -28,27 +20,25 @@ function playRound(playerSelection, computerSelection) {
     else if ((playerSelection === 'rock' && computerSelection === 'scissors') ||
         (playerSelection === 'scissors' && computerSelection === 'paper') ||
         (playerSelection === 'paper' && computerSelection === 'rock')) {
-        console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+        playerScores++;
+        pS.textContent = `Player Score: ${playerScores}`;
     }
     //for losing
     else if ((playerSelection === 'rock' && computerSelection === 'paper') ||
         (playerSelection === 'scissors' && computerSelection === 'rock') ||
         (playerSelection === 'paper' && computerSelection === 'scissors')) {
-        console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+        computerScores++;
+        cS.textContent = `Player Score: ${computerScores}`;
     }
     else {
         //making sure if the player input is valid
-        console.log('Please enter either a rock, scissors, or paper');
+        alert('Please choose either a rock, scissors, or paper');
     }
 }
-
-// const rockJs = document.body.querySelector("#rock");
-// rockJs.addEventListener("click", playRound(rockJs.value));
-// const scissorsJs = document.body.querySelector("#scissors");
-// scissorsJs.addEventListener("click", playRound(scissorsJs.value));
-// const paperJs = document.body.querySelector("#paper");
-// paperJs.addEventListener("click", playRound(paperJs.value));
-
+let playerScores = 0;
+let computerScores = 0;
+const pS = document.body.querySelector('.playerScore');
+const cS = document.body.querySelector('.compScore');
 const buttons = document.body.querySelectorAll('button');
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
@@ -58,16 +48,3 @@ buttons.forEach((button) => {
     })
 }
 );
-
-// function game() {
-//     let i = 0;
-//     // while (i < 5) {
-//     //     //variables for the rps (one user input and another computer generated)
-//     //     let playerSelection = prompt("Please enter rock, paper, or scissors to play."); //changing all to lower case so it's case insensitive;
-//     //     //calling the function to collect the value for the cpu generated value
-//     //     const computerSelection = getComputerChoice();
-//     //     playRound(playerSelection, computerSelection);
-//     //     i++;
-//     // }
-//     //*****fix this part of the code so that it runs 5 times****
-// }
